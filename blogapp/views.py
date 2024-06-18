b35a5e4fe355e7d28e03e40d70f724664c999668
abhinav-appstechy <abhinav@appstechy.com>
2024-06-18 13:16:14 +0530

    updated code

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from blogapp.models import BlogModel
 from django.shortcuts import get_object_or_404
+from django.http import HttpResponse, HttpResponseNotFound
 from django.contrib.sessions.models import Session
 
 # Create your views here.
@@ -18,10 +19,13 @@ def index(request):
     return render(request, "homepage.html", context)
 
 
-def single_blog(request,name):
-    print(name)
-    # blog = get_object_or_404(BlogModel, blog_title=name)
+def single_blog(request, name):
     blog = BlogModel.objects.filter(blog_title__contains=name).first()
+    if blog is None:
+        # Handle the case where no blog with the specified title is found
+        # For example, return a 404 page or redirect to a different view
+        return HttpResponseNotFound("Blog not found")
+
     session_key = request.session.session_key
     if not request.session.get(f'blog_{blog.id}_visited', False):
         # Increment the view count only if the user hasn't visited this page before
@@ -30,11 +34,12 @@ def single_blog(request,name):
         
         # Set a session flag indicating that the user has visited this page
         request.session[f'blog_{blog.id}_visited'] = True
-    print(blog)
+
     context = {
         "blog": blog
     }
     return render(request, "single_blog_page.html", context)
+
 
 def about_us(request):
     context = {
--- blogproject/settings.py
+++ blogproject/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-h&^*n@ir3q-fzxvap9w1y-%_be=1b0re$m)%7b1&+pz7bw$+c3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '*']
 
 
 # Application definition
