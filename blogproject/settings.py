cd7cc3231de486b41985db278cee72f1c754cd31
abhinav-appstechy <abhinav@appstechy.com>
2024-06-18 14:33:47 +0530

    updated code

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 
 # This is the directory where `collectstatic` will collect static files for production
@@ -144,7 +144,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'blog_images')
-MEDIA_URL = "media/"
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
